e83feaf89ec0ab9f2f369cbbe38d3732796f7aa7
_ilia7_ <ilia.moshtaghi83@gmail.com>
2023-10-20 12:17:00 +0330

    Update Coral_Disabler.java

--- src/main/java/me/ilia/coraldisabler/Coral_Disabler.java
+++ src/main/java/me/ilia/coraldisabler/Coral_Disabler.java
@@ -1,6 +1,5 @@
 package me.ilia.coraldisabler;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFadeEvent;
