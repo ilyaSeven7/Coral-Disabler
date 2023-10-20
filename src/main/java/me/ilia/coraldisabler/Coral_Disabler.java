89bbe439fc159354f97893551ff78b0547fd3203
_ilia7_ <ilia.moshtaghi83@gmail.com>
2023-10-20 10:37:46 +0330

    Clean Code.

--- src/main/java/me/ilia/coraldisabler/Coral_Disabler.java
+++ src/main/java/me/ilia/coraldisabler/Coral_Disabler.java
@@ -23,23 +23,7 @@ public final class Coral_Disabler extends JavaPlugin implements Listener {
     }
     @EventHandler
     public void CoralDead(BlockFadeEvent event) {
-        Material x = event.getBlock().getType();
-        Material tb = Material.TUBE_CORAL; //TubeCoral
-        Material fc = Material.FIRE_CORAL; //FireCoral
-        Material brc = Material.BRAIN_CORAL; //BrainCoral
-        Material bc = Material.BUBBLE_CORAL; //Bubble Coral
-        Material hc = Material.HORN_CORAL; //Horn Coral
-        Material tcb = Material.TUBE_CORAL_BLOCK; //TubeCoralBlock
-        Material fcb = Material.FIRE_CORAL_BLOCK; //FireCoralBlock
-        Material brcb = Material.BRAIN_CORAL_BLOCK; //BrainCoralBlock
-        Material bcb = Material.BUBBLE_CORAL_BLOCK; //Bubble CoralBlock
-        Material hcb = Material.HORN_CORAL_BLOCK; //Horn CoralBlock
-        Material tbf = Material.TUBE_CORAL_FAN; //TubeCoral
-        Material fcf = Material.FIRE_CORAL_FAN; //FireCoral
-        Material brcf = Material.BRAIN_CORAL_FAN; //BrainCoral
-        Material bcf = Material.BUBBLE_CORAL_FAN; //Bubble Coral
-        Material hcf = Material.HORN_CORAL_FAN; //Horn Coral
-        if (x == tb || x == fc || x == brc || x == bc || x == hc || x == tcb || x == fcb || x == brcb || x == bcb || x == hcb || x == tbf | x == fcf || x == brcf || x == bcf || x == hcf){
+        if (event.getBlock().getType().toString().contains("CORAL")){
             event.setCancelled(true);
         }
     }
